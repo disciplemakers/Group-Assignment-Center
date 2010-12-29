ae13724cd218cafacb89387099210f80e9bed804
Brian Roberg <robergb@dm.org>
2010-12-29 13:43:20 -0500

    Initial commit: unmodified auto-generated Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gac::Application.config.secret_token = '8fdccad8d24a6073f73372fe85d96fe8359232c3c882a41a0db53ab66b2385b322bc3f8538a3c9473ac5a3669525f0919ad6f8b86a9e15a170ff517cb5efa11e'
