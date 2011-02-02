71fe4f6ae796d756b123ec0888bc5a77cfbe8e2f
Adam Focht and Brian Roberg <adamandbrian@dm.org>
2011-02-02 16:32:39 -0500

    Implemented hierarchical display of locations in location index.

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,2 +1,28 @@
 module LocationsHelper
+  
+  # Returns an li tag representing the location, including sub-lists
+  # representing any children.
+  def print_location(location)
+    output = content_tag(:li, location.name, nil, false)
+    if location.children.size > 0
+      contents = ''
+      location.children.each do |child|
+        contents += print_location(child)
+      end
+      output += content_tag(:ul, contents, nil, false)
+    end
+    output
+  end
+
+
+  #<li>
+   # <td><%= location.name %></td>
+    #<td><%= link_to 'Show', location %></td>
+    #<td><%= link_to 'Edit', edit_location_path(location) %></td>
+    #<td><%= link_to 'Destroy', location, :confirm => 'Are you sure?', :method => :delete %></td>
+  #</li>
+      
+  #end
+    
+
 end
