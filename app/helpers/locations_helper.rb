67c06784f6736507d65b9c9462d235cbdf3470ce
Adam Focht and Brian Roberg <adamandbrian@dm.org>
2011-02-03 09:41:47 -0500

    Re-added the actions (show, edit, new child, destroy) on each location.

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -3,7 +3,15 @@ module LocationsHelper
   # Returns an li tag representing the location, including sub-lists
   # representing any children.
   def print_location(location)
-    output = content_tag(:li, location.name, nil, false)
+    # Print the location itself.
+    output = content_tag(:li, nil, false) do
+      link_to(location.name, location) + ' | ' +
+      link_to('Edit', edit_location_path(location)) + ' | ' +
+      link_to('New Child', new_child_path(location)) + ' | ' +
+      link_to('Destroy', location, :confirm => 'Are you sure?', :method => :delete)
+    end
+             
+    # Print any children in a sub-list.
     if location.children.size > 0
       contents = ''
       location.children.each do |child|
@@ -12,17 +20,6 @@ module LocationsHelper
       output += content_tag(:ul, contents, nil, false)
     end
     output
-  end
-
-
-  #<li>
-   # <td><%= location.name %></td>
-    #<td><%= link_to 'Show', location %></td>
-    #<td><%= link_to 'Edit', edit_location_path(location) %></td>
-    #<td><%= link_to 'Destroy', location, :confirm => 'Are you sure?', :method => :delete %></td>
-  #</li>
-      
-  #end
-    
+  end    
 
 end
