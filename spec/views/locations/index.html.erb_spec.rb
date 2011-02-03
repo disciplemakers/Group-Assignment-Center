bc884eb344c2309b828dc1fc6f480f5d2d983b23
Adam Focht and Brian Roberg <adamandbrian@dm.org>
2011-02-03 09:05:07 -0500

    Updated the spec for the index view to reflect the fact we're using unordered lists instead of a table.

--- spec/views/locations/index.html.erb_spec.rb
+++ spec/views/locations/index.html.erb_spec.rb
@@ -15,6 +15,6 @@ describe "locations/index.html.erb" do
   it "renders a list of locations" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "ul>li", :text => "Name".to_s, :count => 2
   end
 end
