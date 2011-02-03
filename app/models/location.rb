5fc5bf73c1ee20f2eb908c04c47119245834323b
Adam Focht and Brian Roberg <adamandbrian@dm.org>
2011-02-03 10:07:04 -0500

    Added dependent children destroy upon parent destroy.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,5 @@
 class Location < ActiveRecord::Base
   belongs_to :parent, :class_name => "Location", :foreign_key => "parent_id"
-  has_many :children, :class_name => "Location", :foreign_key => "parent_id"
+  has_many :children, :class_name => "Location", 
+           :foreign_key => "parent_id", :dependent => :destroy
 end
