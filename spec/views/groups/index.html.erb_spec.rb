e5a0d1531302bdd5e1d8f1ad2b9c2c213ee80cfa
Adam Focht and Brian Roberg <adamandbrian@dm.org>
2011-02-09 14:18:16 -0500

    Using helper print_tree method with ul/li instead of table.

--- spec/views/groups/index.html.erb_spec.rb
+++ spec/views/groups/index.html.erb_spec.rb
@@ -37,10 +37,6 @@ describe "groups/index.html.erb" do
   it "renders a list of groups" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => 1.to_s, :count => 8
-    assert_select "tr>td", :text => false.to_s, :count => 10
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "Label Text".to_s, :count => 2
+    assert_select "ul>li", :text => "Name | Edit | New Child | Destroy".to_s, :count => 2
   end
 end
