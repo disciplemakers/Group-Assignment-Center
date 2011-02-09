d983b0deccae2542c45df891600f1b1961dc355e
Adam Focht and Brian Roberg <adamandbrian@dm.org>
2011-02-09 14:16:28 -0500

    Refactored print_location helper into print_tree to handle both groups and locations.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  
+  # Returns an li tag representing the given object, including sub lists
+  # representing any children.
+  def print_tree(object)
+    # Print the object itself.
+    output = content_tag(:li, nil, false) do
+      link_to(object.name, object) + ' | ' +
+      link_to('Edit', url_for(object)+"/edit") + ' | ' +
+      link_to('New Child', url_for(object)+"/new") + ' | ' +
+      link_to('Destroy', object, :confirm => 'Are you sure?', :method => :delete)
+    end
+             
+    # Print any children in a sub-list.
+    if object.children.size > 0
+      contents = ''
+      object.children.each do |child|
+        contents += print_tree(child)
+      end
+      output += content_tag(:ul, contents, nil, false)
+    end
+    output
+  end 
+  
 end
--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,25 +1,5 @@
 module LocationsHelper
   
-  # Returns an li tag representing the location, including sub-lists
-  # representing any children.
-  def print_location(location)
-    # Print the location itself.
-    output = content_tag(:li, nil, false) do
-      link_to(location.name, location) + ' | ' +
-      link_to('Edit', edit_location_path(location)) + ' | ' +
-      link_to('New Child', new_child_location_path(location)) + ' | ' +
-      link_to('Destroy', location, :confirm => 'Are you sure?', :method => :delete)
-    end
-             
-    # Print any children in a sub-list.
-    if location.children.size > 0
-      contents = ''
-      location.children.each do |child|
-        contents += print_location(child)
-      end
-      output += content_tag(:ul, contents, nil, false)
-    end
-    output
-  end    
+   
 
 end
