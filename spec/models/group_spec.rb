cbe3e7ba31bdc53a176865d7e246b57f5f3a8c66
Adam Focht and Brian Roberg <adamandbrian@dm.org>
2011-02-11 10:39:27 -0500

    Finished tests on validation and 'inheritance' of membership constraints.

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -30,114 +30,135 @@ describe Group do
     @group.should be_invalid
   end
   
-  describe "whose parent has unique membership constraint enabled" do
+  describe "with parent" do
     before(:each) do
-      @group.parent_id = 1
-      @parent = Group.new(:id => 1, :name => 'Parent')
-      @parent.unique_membership = true
-      Group.stub(:find).with(1).and_return(@parent)
+        @group.parent_id = 1
+        @parent = Group.new(:id => 1, :name => 'Parent')
+        Group.stub(:find).with(1).and_return(@parent)
     end
     
-    it "is valid without unique membership constraint" do
-      @group.unique_membership = nil
-      @group.should be_valid
+    describe "whose parent has unique membership constraint enabled" do
+      before(:each) do
+        @parent.unique_membership = true
+      end
+      
+      it "is valid without unique membership constraint" do
+        @group.unique_membership = nil
+        @group.should be_valid
+      end
+      
+      it "is invalid when unique membership constraint is enabled" do
+        @group.unique_membership = true
+        @group.should be_invalid
+      end
+      
+      it "is invalid when unique membership constraint is disabled" do
+        @group.unique_membership = false
+        @group.should be_invalid
+      end
     end
     
-    it "is invalid when unique membership constraint is enabled" do
-      @group.unique_membership = true
-      @group.should be_invalid
+    
+    describe "whose parent has unique membership constraint disabled" do
+      before(:each) do
+        @parent.unique_membership = false
+      end
+      
+      it "is invalid without unique membership constraint" do
+        @group.unique_membership = nil
+        @group.should be_invalid
+      end
+      
+      it "is valid when unique membership constraint is enabled" do
+        @group.unique_membership = true
+        @group.should be_valid
+      end
+      
+      it "is valid when unique membership constraint is disabled" do
+        @group.unique_membership = false
+        @group.should be_valid
+      end
     end
     
-    it "is invalid when unique membership constraint is disabled" do
-      @group.unique_membership = false
-      @group.should be_invalid
+    describe "whose parent does not have unique membership constraint" do
+      before(:each) do
+        @parent.unique_membership = nil
+      end
+      
+      it "is valid without unique membership constraint" do
+        @group.unique_membership = nil
+        @group.should be_valid
+      end
+      
+      it "is invalid when unique membership constraint is enabled" do
+        @group.unique_membership = true
+        @group.should be_invalid
+      end
+      
+      it "is invalid when unique membership constraint is disabled" do
+        @group.unique_membership = false
+        @group.should be_invalid
+      end
+  
+    end
+    
+    describe "whose parent has required membership constraint enabled" do
+      before(:each) do
+        @parent.required_membership = true
+      end
+  
+      it "is invalid without required membership constraint" do
+        @group.required_membership = nil
+        @group.should be_invalid
+      end
+      
+      it "is invalid when required membership constraint is enabled" do
+        @group.required_membership = true
+        @group.should be_invalid
+      end
+      
+      it "is valid when required membership constraint is disabled" do
+        @group.required_membership = false
+        @group.should be_valid      
+      end
+    end
+  
+    describe "whose parent has required membership constraint disabled" do
+      before(:each) do
+        @parent.required_membership = false
+      end
+      
+      it "is invalid without required membership constraint" do
+        @group.required_membership = nil
+        @group.should be_invalid
+      end  
+      it "is invalid when required membership constraint is enabled" do
+        @group.required_membership = true
+        @group.should be_invalid
+      end
+      it "is valid when required membership constraint is disabled" do
+        @group.required_membership = false
+        @group.should be_valid
+      end
+    end
+    
+    describe "whose parent does not have required membership constraint" do
+      before(:each) do
+        @parent.required_membership = nil
+      end
+      
+      it "is valid without required membership constraint" do
+        @group.required_membership = nil
+        @group.should be_valid
+      end
+      it "is valid when required membership constraint is enabled" do
+        @group.required_membership = true
+        @group.should be_valid
+      end
+      it "is invalid when required membership constraint is disabled" do
+        @group.required_membership = false
+        @group.should be_invalid
+      end       
     end
   end
-  
-  
-  describe "whose parent has unique membership constraint disabled" do
-    before(:each) do
-      @group.parent_id = 1
-      @parent = Group.new(:id => 1, :name => 'Parent')
-      @parent.unique_membership = false
-      Group.stub(:find).with(1).and_return(@parent)
-    end
-    
-    it "is invalid without unique membership constraint" do
-      @group.unique_membership = nil
-      @group.should be_invalid
-    end
-    
-    it "is valid when unique membership constraint is enabled" do
-      @group.unique_membership = true
-      @group.should be_valid
-    end
-    
-    it "is valid when unique membership constraint is disabled" do
-      @group.unique_membership = false
-      @group.should be_valid
-    end
-  end
-  
-  describe "whose parent does not have unique membership constraint" do
-    before(:each) do
-      @group.parent_id = 1
-      @parent = Group.new(:id => 1, :name => 'Parent')
-      @parent.unique_membership = nil
-      Group.stub(:find).with(1).and_return(@parent)
-    end
-    
-    it "is valid without unique membership constraint" do
-      @group.unique_membership = nil
-      @group.should be_valid
-    end
-    
-    it "is invalid when unique membership constraint is enabled" do
-      @group.unique_membership = true
-      @group.should be_invalid
-    end
-    
-    it "is invalid when unique membership constraint is disabled" do
-      @group.unique_membership = false
-      @group.should be_invalid
-    end
-
-  end
-  
-  describe "whose parent has required membership constraint enabled" do
-    before(:each) do
-      @group.parent_id = 1
-      @parent = Group.new(:id => 1, :name => 'Parent')
-      @parent.required_membership = true
-      Group.stub(:find).with(1).and_return(@parent)
-    end
-
-    it "is invalid without required membership constraint" do
-      @group.required_membership = nil
-      @group.should be_invalid
-    end
-    
-    it "is invalid when required membership constraint is enabled" do
-      @group.required_membership = true
-      @group.should be_invalid
-    end
-    
-    it "is valid when required membership constraint is disabled" do
-      @group.required_membership = false
-      @group.should be_valid      
-    end
-  end
-
-  describe "whose parent has required membership constraint disabled" do
-    it "is invalid without required membership constraint"  
-    it "is invalid when required membership constraint is enabled"
-    it "is valid when required membership constraint is disabled"
-  end
-  
-  describe "whose parent does not have required membership constraint" do
-    it "is valid without required membership constraint"  
-    it "is valid when required membership constraint is enabled"
-    it "is invalid when required membership constraint is disabled"       
-  end
-  
 end
