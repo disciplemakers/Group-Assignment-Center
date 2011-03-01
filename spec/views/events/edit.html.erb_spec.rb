2c5b7f41d3a61bd36ae7bcd7e8229985f72d6e1c
Brian Roberg <robergb@dm.org>
2011-03-01 15:28:39 -0500

    Added mocked model for group and stubbed model for location so that the view can look up what it needs.

--- spec/views/events/edit.html.erb_spec.rb
+++ spec/views/events/edit.html.erb_spec.rb
@@ -8,6 +8,15 @@ describe "events/edit.html.erb" do
       :location_id => 1,
       :group_id => 1
     ))
+    
+    # The view looks up the name of the event via the group,
+    # so we need to stub the group.
+    @group = assign(:group, mock_model(Group, :name => "Test Event", :level => 1).as_null_object)
+    Group.stub(:find).with(1).and_return(@group)
+    
+    # We also need a location.
+    location = stub_model(Location, :name => "Test Location")
+    Location.stub(:find).with(1).and_return(location)
   end
 
   it "renders the edit event form" do
