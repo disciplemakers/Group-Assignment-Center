db34a2f27ae533b97e4eeb746c2e8839cab1fe99
Brian Roberg <robergb@dm.org>
2011-03-02 16:12:21 -0500

    Added validator for confirmation number.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,6 @@
 class Person < ActiveRecord::Base
   validates_presence_of :first_name, :last_name
+  validates_numericality_of :confirmation_number, :only_integer => true
   validates_numericality_of :graduation_year, :only_integer => true, :allow_blank => true
   validates_length_of :graduation_year, :is => 4, :allow_blank => true
   validate :gender_must_be_M_or_F
