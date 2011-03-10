028c7d470db5aeeca16e5ed567bc2a98818588f4
Adam Focht and Brian Roberg <adamandbrian@dm.org>
2011-03-10 11:27:44 -0500

    capistrano alteration.

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :db_domain, "mysql-dmz"
 
 role :web, domain                         # Your HTTP server, Apache/etc
 role :app, domain                         # This may be the same as your `Web` server
-role :db,  db_domain, :primary => true    # This is where Rails migrations will run  
+role :db,  domain, :primary => true    # This is where Rails migrations will run  
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
