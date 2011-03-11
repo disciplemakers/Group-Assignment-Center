640731768ec5117b247d285fcd5effdf26a3f731
Adam Focht and Brian Roberg <adamandbrian@dm.org>
2011-03-11 16:28:23 -0500

    Added helper to generate nested_set_options-like output for groups with people.

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -1,2 +1,19 @@
 module AssignmentsHelper
+  def groups_with_people_options(class_or_item, mover = nil)
+    class_or_item = class_or_item.roots if class_or_item.is_a?(Class)
+    items = Array(class_or_item)
+    result = []
+    items.each do |root|
+      root.self_and_descendants.map do |i|
+        if mover.nil? || mover.new_record? || mover.move_possible?(i)
+          result << ["#{'---' * (i.level)}#{i.name}", "group-#{i.id}"]
+          i.people.each do |p|
+              result << ["#{'---' * (i.level+1)}#{p.full_name}", "person-#{p.id}"]
+          end
+        end
+      end
+      result.compact
+    end
+    result
+  end
 end
