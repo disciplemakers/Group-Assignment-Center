40a3e04b8e4472ba10c5f95ccd4244e10b400315
Brian Roberg <robergb@dm.org>
2011-03-15 15:43:48 -0400

    Set the application to display the events/assign view as its homepage.

--- config/routes.rb
+++ config/routes.rb
@@ -84,7 +84,7 @@ Gac::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "events#assign"
 
   # See how all your routes lay out with "rake routes"
 
