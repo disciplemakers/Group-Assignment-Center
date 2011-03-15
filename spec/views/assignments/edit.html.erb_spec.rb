f98396c0d36d501ceca4fe151255445938f5c4c8
Brian Roberg <robergb@dm.org>
2011-03-15 11:52:06 -0400

    Updated the assignment edit spec to reflect the fact that assignments are nested in events.

--- spec/views/assignments/edit.html.erb_spec.rb
+++ spec/views/assignments/edit.html.erb_spec.rb
@@ -6,15 +6,16 @@ describe "assignments/edit.html.erb" do
       :group_id => 1,
       :person_id => 1
     ))
+    @event = stub_model(Event, :event_id => 1)
   end
 
   it "renders the edit assignment form" do
     render
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form", :action => assignment_path(@assignment), :method => "post" do
-      assert_select "input#assignment_group_id", :name => "assignment[group_id]"
-      assert_select "input#assignment_person_id", :name => "assignment[person_id]"
+    assert_select "form", :action => edit_event_assignment_path(@event, @assignment), :method => "post" do
+      assert_select "select#_left_side", :name => "[left_side[]]"
+      assert_select "select#assignment_person", :name => "assignment[person][]"
     end
   end
 end
