84f0b381420167bccb2e092d459b184efc6f7aa9
Brian Roberg <robergb@dm.org>
2011-03-15 10:52:18 -0400

    Updated the assignment routing spec to reflect the fact that assignments are nested in events.

--- spec/routing/assignments_routing_spec.rb
+++ spec/routing/assignments_routing_spec.rb
@@ -4,31 +4,31 @@ describe AssignmentsController do
   describe "routing" do
 
     it "recognizes and generates #index" do
-      { :get => "/assignments" }.should route_to(:controller => "assignments", :action => "index")
+      { :get => "/events/1/assignments" }.should route_to(:controller => "assignments", :event_id => "1", :action => "index")
     end
 
     it "recognizes and generates #new" do
-      { :get => "/assignments/new" }.should route_to(:controller => "assignments", :action => "new")
+      { :get => "/events/1/assignments/new" }.should route_to(:controller => "assignments", :event_id => "1", :action => "new")
     end
 
     it "recognizes and generates #show" do
-      { :get => "/assignments/1" }.should route_to(:controller => "assignments", :action => "show", :id => "1")
+      { :get => "/events/1/assignments/1" }.should route_to(:controller => "assignments", :event_id => "1", :action => "show", :id => "1")
     end
 
     it "recognizes and generates #edit" do
-      { :get => "/assignments/1/edit" }.should route_to(:controller => "assignments", :action => "edit", :id => "1")
+      { :get => "/events/1/assignments/1/edit" }.should route_to(:controller => "assignments", :event_id => "1", :action => "edit", :id => "1")
     end
 
     it "recognizes and generates #create" do
-      { :post => "/assignments" }.should route_to(:controller => "assignments", :action => "create")
+      { :post => "/events/1/assignments" }.should route_to(:controller => "assignments", :event_id => "1", :action => "create")
     end
 
     it "recognizes and generates #update" do
-      { :put => "/assignments/1" }.should route_to(:controller => "assignments", :action => "update", :id => "1")
+      { :put => "/events/1/assignments/1" }.should route_to(:controller => "assignments", :event_id => "1", :action => "update", :id => "1")
     end
 
     it "recognizes and generates #destroy" do
-      { :delete => "/assignments/1" }.should route_to(:controller => "assignments", :action => "destroy", :id => "1")
+      { :delete => "/events/1/assignments/1" }.should route_to(:controller => "assignments", :event_id => "1", :action => "destroy", :id => "1")
     end
 
   end
