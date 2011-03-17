6cce1be9ec9a2b7e0523226fac3833b13427e54f
Adam Focht <fochta@dm.org>
2011-03-17 16:12:35 -0400

    Updated ordering in relational definition.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,5 @@ class Event < ActiveRecord::Base
   belongs_to :location
   belongs_to :status
   belongs_to :group, :dependent => :destroy
-  has_many :people, :dependent => :destroy
+  has_many :people, :dependent => :destroy, :order => "people.last_name, people.first_name"
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -5,7 +5,7 @@ class Group < ActiveRecord::Base
   belongs_to :location
   has_one :event
   has_many :assignments, :dependent => :destroy
-  has_many :people, :through => :assignments
+  has_many :people, :through => :assignments, :order => "people.last_name, people.first_name"
   
   validates_presence_of :name
   validates_numericality_of :capacity, :only_integer => true,
