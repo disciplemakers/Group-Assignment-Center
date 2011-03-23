23a230beff85f1e2722aacaf527ffc26f7a4ceb1
Adam Focht and Brian Roberg <adamandbrian@dm.org>
2011-03-23 16:41:18 -0400

    Added a few more stubs, trying to get other tests to pass.

--- spec/views/assignments/edit.html.erb_spec.rb
+++ spec/views/assignments/edit.html.erb_spec.rb
@@ -2,9 +2,16 @@ require 'spec_helper'
 
 describe "assignments/edit.html.erb" do
   before(:each) do
+    @group = stub_model(Group, :id => 1, 
+                        :required_membership_scope => self,
+                        :self_and_descendants => [self],
+                        :people => [stub_model(Person)],
+                        :capacity => 4)
+
     @assignment = assign(:assignment, stub_model(Assignment,
       :group_id => 1,
-      :person_id => 1
+      :person_id => 1,
+      :group => @group
     ))
     @event = stub_model(Event, :event_id => 1)
   end
