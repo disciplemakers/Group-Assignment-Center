b4b98cd9dc53fd30ff37f527bb9ff1f229a30823
Adam Focht <fochta@dm.org>
2011-03-31 11:51:34 -0400

    Added Code to group observer to watch for updates and update assigned people's label_text field.

--- app/models/group_observer.rb
+++ app/models/group_observer.rb
@@ -1,7 +1,15 @@
-require 'pp'
-
 class GroupObserver < ActiveRecord::Observer
-  def after_update(group)
-    pp group.people
+  observe :group
+  
+  def after_save(group)
+    label_text = group.build_custom_field_text
+    label_field = group.custom_field.people_field
+    
+    group.people.each do |person|
+      person.update_attribute(label_field, label_text)
+      #person.write_custom_field_to_remote(RegonlineConnector.new(session[:account_id], session[:username], session[:password]),
+      #                                    group.custom_field.name,
+      #                                    label_text) #if RAILS_ENV == "production"
+    end
   end
 end
