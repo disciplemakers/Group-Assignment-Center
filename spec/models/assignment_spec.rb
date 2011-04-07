56db152feb9c8ec328ac43d4e562db1ea4c7ca80
Adam Focht <fochta@dm.org>
2011-04-07 09:54:47 -0400

    Assignment model tests.

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -4,8 +4,12 @@ describe Assignment do
   before(:each) do
     @assignment = stub_model(Assignment, :group_id  => '1',
                                          :person_id => '37')
-    @group = stub_model(Group, :id => "1")
-    @person = stub_model(Person, :id => "37")
+    @group = stub_model(Group, :id => "1", :can_contain_people => true, :capacity => nil)
+    @group.stub(:self_and_ancestors).with(no_args()).and_return([@group])
+    @assignment.stub(:group).with(no_args()).and_return(@group)
+    @person = stub_model(Person, :id => "37", :first_name => "John", :last_name => "Doe")
+    @person.stub(:full_name).with(no_args()).and_return("John Doe")
+    @assignment.stub(:person).with(no_args()).and_return(@person)
   end
   
   it "is invalid without a group_id" do
@@ -20,16 +24,22 @@ describe Assignment do
   
   describe "to a group that has a capacity" do
     before(:each) do
-      @group.capacity = 7
+      @cap_assignment = stub_model(Assignment, :group_id  => '1',
+                                               :person_id => '37')
+      @cap_group = stub_model(Group, :id => "1", :capacity => 7, :can_contain_people => true)
+      @cap_group.stub(:self_and_ancestors).with(no_args()).and_return([@cap_group])
+      @cap_assignment.stub(:group).with(no_args()).and_return(@cap_group)
+      @cap_assignment.stub(:person).with(no_args()).and_return(@person)
     end
+    
     it "is invalid if the group is already at capacity" do
-      @group.stub(:people).and_return(%w{ john paul george ringo tom dick harry })
-      @assignment.should be_invalid
+      @cap_group.stub(:people).and_return(%w{ john paul george ringo tom dick harry })
+      @cap_assignment.should be_invalid
     end
     
     it "is valid if the group is below capacity" do
-      @group.stub(:people).and_return(%w{ john paul george ringo })
-      @assignment.should be_valid
+      @cap_group.stub(:people).and_return(%w{ john paul george ringo })
+      @cap_assignment.should be_valid
     end
   end
   
