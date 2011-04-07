f61a7ec2cf30328ba864ec07018153ed54b64359
Adam Focht <fochta@dm.org>
2011-04-07 11:33:22 -0400

    Assignments helper factoring and testing.

--- app/helpers/assignments_helper.rb
+++ app/helpers/assignments_helper.rb
@@ -1,38 +1,14 @@
 module AssignmentsHelper
-  def groups_with_people_options(class_or_item, mover = nil)
-    class_or_item = class_or_item.roots if class_or_item.is_a?(Class)
-    items = Array(class_or_item)
-    result = []
-    items.each do |root|
-      root.self_and_descendants.map do |i|
-        if mover.nil? || mover.new_record? || mover.move_possible?(i)
-          result << ["#{'---' * (i.level)}#{i.name}", "group-#{i.id}"]
-          i.people.each do |p|
-              result << ["#{'---' * (i.level+1)}#{p.full_name_and_info}",
-                         "person-#{p.id}--group-#{i.id}"]
-          end
-        end
-      end
-      result.compact
-    end
-    result
-  end
-  
-  def groups_with_people_options_with_style(class_or_item, mover = nil)
-    class_or_item = class_or_item.roots if class_or_item.is_a?(Class)
-    items = Array(class_or_item)
+  def groups_with_people_options(group)
+    return "" if group.nil?
     options_for_select = []
-    items.each do |root|
-      root.self_and_descendants.map do |i|
-        if mover.nil? || mover.new_record? || mover.move_possible?(i)
-          count = i.people.count
-          capacity = i.capacity
-          status = (capacity.nil? ? (count <= 0 ? '' : " (#{count})") : " (#{count}/#{capacity})")   
-          options_for_select << %(<option value="group-#{html_escape(i.id.to_s)}" class="group-option">#{'---' * (i.level)}#{i.name}#{status}</option>)
-          i.people.each do |p|
-              options_for_select << %(<option value="person-#{p.id}--group-#{i.id}" class="person-option">#{'----' * (i.level+1)}#{p.full_name_and_info}</option>)
-          end
-        end
+    group.self_and_descendants.each do |i|
+      count = i.people.count
+      capacity = i.capacity
+      status = (capacity.nil? ? (count <= 0 ? '' : " (#{count})") : " (#{count}/#{capacity})")   
+      options_for_select << %(<option value="group-#{html_escape(i.id.to_s)}" class="group-option">#{'---' * (i.level)}#{i.name}#{status}</option>)
+      i.people.each do |p|
+          options_for_select << %(<option value="person-#{p.id}--group-#{i.id}" class="person-option">#{'----' * (i.level+1)}#{p.full_name_and_info}</option>)
       end
     end
     options_for_select.join("\n")
--- spec/helpers/assignments_helper_spec.rb
+++ spec/helpers/assignments_helper_spec.rb
@@ -11,5 +11,71 @@ require 'spec_helper'
 #   end
 # end
 describe AssignmentsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @group = mock_model(Group,
+                        :id                                => 1,
+                        :name                              => "Parent",
+                        :capacity                          => 1,
+                        :people                            => [],
+                        :can_contain_people                => false,
+                        :can_contain_groups                => true,
+                        :location_id                       => 1,
+                        :comment                           => "MyText",
+                        :unique_membership                 => false,
+                        :required_membership               => false,
+                        :gender_constraint_id              => 1,
+                        :label_text                        => "Label Text",
+                        :label_text_prepend_to_child_label => false,
+                        :label_field                       => 1,
+                        :level                             => 1)
+    @people = [mock_model(Person,
+                          :id                 => 1,
+                          :full_name_and_info => "Full Name and Info 1"),
+               mock_model(Person,
+                          :id                 => 2,
+                          :full_name_and_info => "Full Name and Info 2")]
+    @descendant = mock_model(Group,
+                             :id                                => 2,
+                             :name                              => "Child",
+                             :parent_id                         => 1,
+                             :capacity                          => 2,
+                             :people                            => @people,
+                             :can_contain_people                => true,
+                             :can_contain_groups                => false,
+                             :location_id                       => 1,
+                             :comment                           => "MyText",
+                             :unique_membership                 => false,
+                             :required_membership               => false,
+                             :gender_constraint_id              => 1,
+                             :label_text                        => "Label Text",
+                             :label_text_prepend_to_child_label => false,
+                             :label_field                       => 1,
+                             :level                             => 2)
+  end
+  
+  describe "when asking for groups with people select options" do
+    describe "when given an object" do
+      it "should return a string of html safe options" do
+        @group.stub(:self_and_descendants).with(no_args()).and_return([@group, @descendant])
+        @descendant.stub(:self_and_descendants).with(no_args()).and_return([@descendant])
+        return_string = "<option value=\"group-1\" class=\"group-option\">---Parent (0/1)</option>\n" +
+                        "<option value=\"group-2\" class=\"group-option\">------Child (2/2)</option>\n" +
+                        "<option value=\"person-1--group-2\" class=\"person-option\">------------Full Name and Info 1</option>\n" +
+                        "<option value=\"person-2--group-2\" class=\"person-option\">------------Full Name and Info 2</option>"
+
+        groups_with_people_options(@group).should == return_string
+      end
+      it "should return an empty string when there are no options to present" do
+        @group.stub(:self_and_descendants).with(no_args()).and_return([])
+        
+        groups_with_people_options(@group).should == ""
+      end
+    end
+    
+    describe "when given a nil object" do
+      it "should return an empty string" do
+        groups_with_people_options(nil).should == ""
+      end
+    end
+  end  
 end
