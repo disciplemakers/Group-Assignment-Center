c65fddf417f0a549bb65dc9c5f68c3e6ba44865e
Adam Focht <fochta@dm.org>
2011-04-07 09:54:01 -0400

    Moved session/login redirection test from application controller to events controller and removed unnecessary events controller tests.

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,13 +1,5 @@
 require 'spec_helper'
 
 describe ApplicationController do
-  describe "without session information" do
-    before(:each) do
-      session[:username] = nil
-      session[:password] = nil
-    end
-    it "should redirect to the login page" do
-      { :get => "/events" }.should redirect_to(login_url)
-    end
-  end
+
 end
